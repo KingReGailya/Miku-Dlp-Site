717a93da80790cc8647e53f9f6a837822d335075
KingReGailya <gailregalia@gmail.com>
2022-10-18 22:29:24 -0400

    version 0.1

--- dockerfile
+++ dockerfile
@@ -5,13 +5,14 @@ RUN apt update
 #install dependises 
 
 RUN apt install apt-utils
-RUN apt -y install nano curl python3 ffmpeg php php-fpm php-mysql
+RUN apt -y install nano curl python3 ffmpeg 
 RUN apt-get update && apt-get install -y git
 
 #installs YT-DLP
 
 RUN curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-
 RUN chmod a+rx /usr/local/bin/yt-dlp
+
+RUN mkdir /downloads
 
 COPY src/ /var/www/html
--- src/main.php
+++ src/main.php
@@ -1,10 +1,53 @@
 <?php
 
+$source = $_POST["source"];
+$format = $_POST["format"];
 $link = $_POST["link"];
+$id = rand(0,1000);
 
-print($link);
 
-shell_exec("yt-dlp -f 'ba' -x --audio-format mp3 '".$link."'");
+function download($format, $link, $id) {
+
+    shell_exec("yt-dlp -f 'ba' '".$format."' '".$link."' -o '/downloads/'".$id."'/%(title)s.%(ext)s'");
+    print($format);
+    print($link);
+
+}
+
+#print($id )
+#print($link );
+#print($format );
+#print($source );
+
+switch ($format) {
+    case 'Mp3':
+        $format = "-x --audio-format mp3";
+        download($format,$link,$id);
+        #print($id);
+        break;
+
+    case 'Mp4':
+        $format = "-S res,ext:mp4:m4a --recode mp4";
+        download($format,$link,$id);
+        break;
+}
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<p> Download complete</p>
+<a href="/downloads/<?php echo $id;?>/*" download>
+  <img src="../php.png" alt="W3Schools" width="104" height="142">
+</a>
+</body>
+</html>
